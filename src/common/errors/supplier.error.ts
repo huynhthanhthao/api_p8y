559dfe03b05bacfa1346eb70022a5b67d9903961
Huynh Thanh Thao <httharo@gmail.com>
2025-09-15 17:11:42 +0700

    fix: some code error

---
+++ src/common/errors/supplier.error.ts
@@ -0,0 +1,18 @@
+export const SUPPLIER_ERROR = {
+  SUPPLIER_NOT_FOUND: {
+    code: 'SUPPLIER_01',
+    message: 'Nhà cung cấp không tồn tại'
+  },
+  SOME_SUPPLIERS_NOT_FOUND: {
+    code: 'SUPPLIER_02',
+    message: 'Một số nhà cung cấp không tồn tại'
+  },
+  PHONE_ALREADY_EXISTS: {
+    code: 'SUPPLIER_03',
+    message: 'Số điện thoại đã tồn tại'
+  },
+  CODE_ALREADY_EXISTS: {
+    code: 'SUPPLIER_04',
+    message: 'Mã nhà cung cấp đã tồn tại'
+  }
+}
