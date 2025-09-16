b487bc9b9881a382033ad565c5094631cd33e3a1
Huynh Thanh Thao <httharo@gmail.com>
2025-09-16 08:53:49 +0700

    feat: add err code update file

--- src/common/errors/file.error.ts
+++ src/common/errors/file.error.ts
@@ -2,5 +2,9 @@ export const FILE_ERROR = {
   FILE_NOT_FOUND: {
     code: 'FILE_01',
     message: 'Không tìm thấy file'
+  },
+  FILE_INVALID: {
+    code: 'FILE_02',
+    message: 'File không hợp lệ'
   }
 }
